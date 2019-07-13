9713fa873c866336f9701b084d65ad78393d16e5
Jake Gealer <jake@gealer.email>
2019-07-13 13:09:32 +0100

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu:19.04
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get update && apt-get install -y apache2
+EXPOSE 80
+COPY 000-default.conf /etc/apache2/sites-available/000-default.conf
+COPY .  /var/www/public
+RUN rm /var/www/public/000-default.conf
+CMD /usr/sbin/apache2ctl -D FOREGROUND
---
+++ core.js
@@ -0,0 +1,229 @@
+"use strict"
+
+// Sets record information.
+const records = {
+    A: {
+        info: "A records are the most basic type of DNS record and are used to point a domain or subdomain to an IPv4 address.",
+        url: "https://kb.pressable.com/article/dns-record-types-explained/",
+        expectsIp: true,
+    },
+    TXT: {
+        info: "TXT records are a type of DNS record that contains text information for sources outside of your domain.",
+        url: "https://support.google.com/a/answer/2716800?hl=en",
+        additionalDataParsing: data => data.substr(1).slice(0, -1),
+    },
+    MX: {
+        info: "A mail exchanger record (MX record) specifies the mail server responsible for accepting email messages on behalf of a domain name.",
+        url: "https://en.wikipedia.org/wiki/MX_record",
+    },
+    AAAA: {
+        info: "AAAA records behave the same as A records but for IPv6. They are used to point a domain or subdomain to a IPv6 address.",
+        url: "https://help.fasthosts.co.uk/app/answers/detail/a_id/1548/~/dns-aaaa-records",
+        expectsIp: true,
+    },
+    CAA: {
+        info: "CAA records allow domain owners to specify which Certificate Authorities (CAs) are permitted to issue certificates.",
+        url: "https://www.digitalocean.com/docs/networking/dns/how-to/caa/",
+    },
+    NS: {
+        info: "NS stands for \"name server\" and this record indicates which DNS server is authoritative for that domain (which server contains the actual DNS records). A domain will often have multiple NS records which can indicate primary and backup name servers for that domain.",
+        url: "https://www.cloudflare.com/learning/dns/dns-records/dns-ns-record/",
+    },
+    SRV: {
+        info: "A Service record (SRV record) is a specification of data in the Domain Name System defining the location, i.e. the hostname and port number, of servers for specified services. It is defined in RFC 2782, and its type code is 33.",
+        url: "https://en.wikipedia.org/wiki/SRV_record",
+    },
+}
+
+// Sanitizes the external input. Never trust external input!
+const sanitize = data => {
+    const lt = /</g, 
+        gt = />/g, 
+        ap = /'/g, 
+        ic = /"/g
+
+    return data.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;")
+}
+
+// Gets the domain input element.
+const domainInput = document.getElementById("DomainInput")
+
+// Runs the WHOIS lookup.
+const whoisLookup = async (spanId, ip) => {
+    const url = `https://whois.arin.net/rest/ip/${ip}.json`
+    const fetchRes = await fetch(
+        url, {
+            headers: {
+                Accept: "application/json",
+            },
+        }
+    )
+    const json = await fetchRes.json()
+    const noWhoisData = "<p>No WHOIS data found.</p>"
+    if (!json.net.rdapRef.$) {
+        return noWhoisData
+    }
+    const rdapFetch = await fetch(json.net.rdapRef.$)
+    if (!rdapFetch.ok) {
+        return noWhoisData
+    }
+    const rdapFetchJson = await rdapFetch.json()
+    let remarks = ""
+    for (const remark of rdapFetchJson.remarks ? rdapFetchJson.remarks : []) {
+        remarks += `<p style="font-size: 11px">${remark.description}</p>`
+    }
+    let owner = rdapFetchJson.name
+    if (rdapFetchJson.entities[0]) {
+        owner = `${rdapFetchJson.entities[0].vcardArray[1][1][3]} (${owner})`
+    }
+    document.getElementById(spanId).innerHTML = `
+        <p style="font-size: 11px"><b>Owner:</b> ${owner}</p>
+        ${remarks}
+    `
+}
+
+// Gets the DNS record.
+const getDNSRecord = async (key, text) => {
+    let html = `
+        <h3 class="title is-3">${key} Records</h3>
+        <p>${records[key].info} <a href="${records[key].url}">Learn more</a></p>
+    `
+    const fetchRes = await fetch(
+        `https://cloudflare-dns.com/dns-query?name=${encodeURIComponent(text)}&type=${key}`,
+        {
+            headers: {
+                Accept: "application/dns-json",
+            },
+        }
+    )
+    if (!fetchRes.ok) {
+        throw fetchRes
+    }
+    const json = await fetchRes.json()
+    if (!json.Answer) {
+        html += "<p><b>Could not find any records of this type.</b></p>"
+    } else {
+        const recordsJoined = {}
+        if (key === "MX") {
+            for (const record of json.Answer) {
+                const dataSplit = record.data.split(" ")
+                record.data = dataSplit[1]
+                record.priority = Number(dataSplit[0])
+            }
+        }
+        json.Answer.sort((a, b) => {
+            if (a.priority) {
+                return a.priority - b.priority
+            }
+            if (a.TTL) {
+                return b.TTL - a.TTL
+            }
+        })
+        for (const record of json.Answer) {
+            delete record.type
+            const recordKeys = Object.keys(record)
+            for (const recordKeyOrigin of recordKeys) {
+                const recordKey = `${recordKeyOrigin[0].toUpperCase()}${recordKeyOrigin.substr(1)}`
+                if (recordsJoined[recordKey]) {
+                    recordsJoined[recordKey].push(record[recordKeyOrigin])
+                } else {
+                    recordsJoined[recordKey] = [record[recordKeyOrigin]]
+                }
+            }
+        }
+        const keys = Object.keys(recordsJoined)
+        let headings = "<thead><tr>"
+        for (const heading of keys) {
+            headings += `
+                <th>${heading}</th>
+            `
+        }
+        headings += "</tr></thead>"
+        let body = "<tbody>"
+        let largestRecordPart = 0
+        for (const part of Object.values(recordsJoined)) {
+            if (part.length > largestRecordPart) {
+                largestRecordPart = part.length
+            }
+        }
+        for (let i = 0; i < largestRecordPart; i++) {
+            let row = "<tr>"
+            for (const collectionKey of keys) {
+                let item = recordsJoined[collectionKey][i]
+                if (item === undefined) {
+                    item = "--"
+                } else if (collectionKey === "Data") {
+                    item = records[key].additionalDataParsing ? records[key].additionalDataParsing(item.toString()) : item.toString()
+                } else if (collectionKey === "Name") {
+                    const last = item[item.length - 1]
+                    if (last === ".") {
+                        item = item.slice(0, -1)
+                    }
+                } else {
+                    item = item.toString()
+                }
+                let whois = ""
+                if (records[key].expectsIp && collectionKey === "Data") {
+                    const spanId = Math.random().toString()
+                    whoisLookup(spanId, item)
+                    whois = `<hr style="margin: 5px"><span id="${spanId}"><p style="font-size: 11px"><i>Loading WHOIS data...</i></p></span>`
+                }
+                row += `<td>${sanitize(item)}${whois}</td>`
+            }
+            row += "</tr>"
+            body += row
+        }
+        body += "</tbody>"
+        html += `
+            <br>
+            <table class="table is-bordered">
+                ${headings}
+                ${body}
+            </table>
+        `
+    }
+    html += "<hr>"
+    return html
+}
+
+// Does the main DNS searching.
+const searchDNS = async() => {
+    const text = domainInput.value
+    const promises = []
+    for (const key of Object.keys(records)) {
+        promises.push(getDNSRecord(key, text))
+    }
+    const allHtml = await Promise.all(promises)
+    document.getElementById("content").innerHTML = allHtml.join("")
+}
+
+// Gets the search button element.
+const searchButton = document.getElementById("SearchButton")
+
+// Focuses the domain text box.
+window.onload = () => domainInput.focus()
+
+// Defines if a search is running.
+let running = false
+
+// Runs the main code block to get DNS results.
+const searchDNSEvent = async() => {
+    if (running) {
+        return
+    }
+    running = true
+    try {
+        searchButton.classList.add("is-loading")
+        await searchDNS()
+        searchButton.classList.remove("is-loading")
+    } finally {
+        running = false
+    }
+}
+
+// Run the searchDNSEvent function when ENTER is detected in the text box.
+domainInput.addEventListener("keyup", event => {
+    if (event.key === "Enter") {
+        searchDNSEvent()
+    }
+})
