b2a6867f45cdbab55bb95c5394108e7d4e4d753f
Jake Gealer <jake@gealer.email>
2019-07-18 11:54:55 +0100

    Make \n work for record descriptions (related to #1)

--- core.js
+++ core.js
@@ -3,7 +3,7 @@
 // Sets record information.
 const records = {
     A: {
-        info: "A records are the most basic type of DNS record and are used to point a domain or subdomain to an IPv4 address. <b>These do NOT act as MX records, to receive e-mail, you need to set a MX record.</b>",
+        info: "A records are the most basic type of DNS record and are used to point a domain or subdomain to an IPv4 address.\n<b>These do NOT act as MX records, to receive e-mail, you need to set a MX record.</b>",
         url: "https://kb.pressable.com/article/dns-record-types-explained/",
         expectsHost: true,
     },
@@ -19,7 +19,7 @@ const records = {
         expectsHost: true,
     },
     AAAA: {
-        info: "AAAA records behave the same as A records but for IPv6. They are used to point a domain or subdomain to a IPv6 address.",
+        info: "AAAA records behave the same as A records but for IPv6.\nThey are used to point a domain or subdomain to a IPv6 address.",
         url: "https://help.fasthosts.co.uk/app/answers/detail/a_id/1548/~/dns-aaaa-records",
         expectsHost: true,
     },
@@ -28,13 +28,13 @@ const records = {
         url: "https://www.digitalocean.com/docs/networking/dns/how-to/caa/",
     },
     NS: {
-        info: "NS stands for \"name server\" and this record indicates which DNS server is authoritative for that domain (which server contains the actual DNS records). A domain will often have multiple NS records which can indicate primary and backup name servers for that domain.",
+        info: "NS stands for \"name server\" and this record indicates which DNS server is authoritative for that domain (which server contains the actual DNS records).\nA domain will often have multiple NS records which can indicate primary and backup name servers for that domain.",
         url: "https://www.cloudflare.com/learning/dns/dns-records/dns-ns-record/",
         additionalDataParsing: data => data.endsWith(".") ? data.slice(0, -1) : data,
         expectsHost: true,
     },
     SRV: {
-        info: "A Service record (SRV record) is a specification of data in the Domain Name System defining the location, i.e. the hostname and port number, of servers for specified services. It is defined in RFC 2782, and its type code is 33.",
+        info: "A Service record (SRV record) is a specification of data in the Domain Name System defining the location, i.e. the hostname and port number, of servers for specified services.\nIt is defined in RFC 2782, and its type code is 33.",
         url: "https://en.wikipedia.org/wiki/SRV_record",
     },
 }
@@ -246,7 +246,7 @@ const whoisLookup = async (spanId, ip) => {
 const getDNSRecord = async (key, text) => {
     let html = `
         <h3 class="title is-3" id="${key}-Records">${key} Records <a href="#${key}-Records"><i class="fas fa-link" style="color: black; font-size: 50%;"></i></a></h3>
-        <p>${records[key].info} <a href="${records[key].url}">Learn more</a></p>
+        <p>${records[key].info.replace(/\n/g, "<br>")} <a href="${records[key].url}">Learn more</a></p>
     `
     const fetchRes = await fetch(
         `https://cloudflare-dns.com/dns-query?name=${encodeURIComponent(text)}&type=${key}`,
@@ -398,7 +398,7 @@ const getDNSRecord = async (key, text) => {
                     }
                 } else if (key === "TXT") {
                     if (tSplit && tSplit.length > 1 && txtFragments[tSplit[0]]) {
-                        extra += `<hr style="margin: 5px"><p style="font-size: 11px"><b>${txtFragments[tSplit[0]]}</b></p>`
+                        extra += `<hr style="margin: 5px"><p style="font-size: 11px"><b>${txtFragments[tSplit[0]].replace(/\n/g, "<br>")}</b></p>`
                     }
                 }
                 row += `<td>${item}${extra}</td>`
