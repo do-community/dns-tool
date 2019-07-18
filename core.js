ac1cddcb313a012b4bd28ada890560da490dc976
Jake Gealer <jake@gealer.email>
2019-07-18 11:58:19 +0100

    Shrunk all the explanations (related to #1)

--- core.js
+++ core.js
@@ -42,8 +42,8 @@ const records = {
 // Defines TXT fragments and what they mean.
 const globalsign = "This TXT record is used so that GlobalSign can verify that they are issuing certificates to the domain owner."
 const txtFragments = {
-    "google-site-verification": "TXT records related to Google site verification are used by Google as validation to link a domain to a user for various domain-related Google services.",
-    "mailru-verification": "TXT records related to mail.ru are used to tie a domain to a user. This allows you to check statistics for your domain related to mail.ru users.",
+    "google-site-verification": "This TXT record is used so that Google can verify the domain owner.",
+    "mailru-verification": "This TXT record is used so that mail.ru can verify the domain owner.",
     "MS": "This TXT record is used for Office 365 domain verification.",
     "keybase-site-verification": "This TXT record is commonly used to verify that a Keybase user is in ownership of a domain.",
     "_globalsign-domain-verification": globalsign,
@@ -52,10 +52,10 @@ const txtFragments = {
     "status-page-domain-verification": "This is used so that Statuspage.io can verify the domain owner.",
     "segment-site-verification": "This is used so that Segment.com can verify the domain owner.",
     "logmein-verification-code": "This is used so that LogMeIn can verify the domain owner.",
-    "facebook-domain-verification": "This is used so that Facebook can verify the domain owner so they can provide domain statistics.",
+    "facebook-domain-verification": "This is used so that Facebook can verify the domain owner.",
     "yandex-verification": "This is used so that Yandex can verify the domain owner.",
     "loaderio": "This is used so that Loader.io can verify the domain owner.",
-    "v": "This is a SPF record which is used to identify trusted sources for transmitted e-mail. This makes it easier for recievers to tell if a e-mail is spam.",
+    "v": "This is a SPF record which is used to identify trusted sources for transmitted e-mail.\nThis makes it easier for recievers to tell if a e-mail is spam.",
 }
 
 // Defines all included IP blacklists.
