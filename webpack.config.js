7b808d41899fdccf97c9a37e22229a877a00964e
Jake Gealer <jake@gealer.email>
2019-07-23 16:26:47 +0100

    Get parceljs working

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
