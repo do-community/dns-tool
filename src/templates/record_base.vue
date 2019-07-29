97b984c3c8cf3d1f70adae5d2b6b68f031fd9d8a
Jake Gealer <jake@gealer.email>
2019-07-29 15:31:03 +0100

    New linting rules

--- src/templates/record_base.vue
+++ src/templates/record_base.vue
@@ -1,60 +1,60 @@
 <template>
-  <span>
-    <span v-for="record in records">
-      <Record
-        :ns="ns"
-        :record-type="record.name"
-        :record-url="record.url"
-        :record-description="record.description"
-        :data="$props.data"
-        :expects-host="record.expectsHost"
-      />
+    <span>
+        <span v-for="record in records">
+            <Record
+                :ns="ns"
+                :record-type="record.name"
+                :record-url="record.url"
+                :record-description="record.description"
+                :data="$props.data"
+                :expects-host="record.expectsHost"
+            />
+        </span>
     </span>
-  </span>
 </template>
 
 <script>
-import Record from "./record"
-import recordsDataset from "../data/records"
-import cfDNS from "../utils/cfDNS"
+    import Record from "./record"
+    import recordsDataset from "../data/records"
+    import cfDNS from "../utils/cfDNS"
 
-export default {
-    name: "RecordBase",
-    components: {
-        Record,
-    },
-    props: {
-        data: String,
-    },
-    data() {
-        const setRecords = []
-        for (const recordKey in recordsDataset) {
-            const record = recordsDataset[recordKey]
-            setRecords.push({
-                name: recordKey,
-                description: record.info,
-                url: record.url,
-                expectsHost: Boolean(record.expectsHost),
-            })
-        }
-        return {
-            records: setRecords,
-            ns: "",
-        }
-    },
-    watch: {
+    export default {
+        name: "RecordBase",
+        components: {
+            Record,
+        },
+        props: {
+            data: String,
+        },
         data() {
-            this.recordsInit()
+            const setRecords = []
+            for (const recordKey in recordsDataset) {
+                const record = recordsDataset[recordKey]
+                setRecords.push({
+                    name: recordKey,
+                    description: record.info,
+                    url: record.url,
+                    expectsHost: Boolean(record.expectsHost),
+                })
+            }
+            return {
+                records: setRecords,
+                ns: "",
+            }
         },
-    },
-    async mounted() {
-        await this.recordsInit()
-    },
-    methods: {
-        async recordsInit() {
-            const json = await (await cfDNS(this.$props.data, "NS")).json()
-            this.$data.ns = json.Answer ? json.Answer[0].data : ""
+        watch: {
+            data() {
+                this.recordsInit()
+            },
         },
-    },
-}
+        async mounted() {
+            await this.recordsInit()
+        },
+        methods: {
+            async recordsInit() {
+                const json = await (await cfDNS(this.$props.data, "NS")).json()
+                this.$data.ns = json.Answer ? json.Answer[0].data : ""
+            },
+        },
+    }
 </script>
--- src/templates/truncated_record.vue
+++ src/templates/truncated_record.vue
@@ -1,28 +1,28 @@
 <template>
-  <span v-if="truncate">
-    <p>{{ this.$props.truncated }} <a @click="toggleTruncation">Show more...</a></p>
-  </span>
-  <span v-else>
-    <p>{{ this.$props.value }} <a @click="toggleTruncation">Show less...</a></p>
-  </span>
+    <span v-if="truncate">
+        <p>{{ this.$props.truncated }} <a @click="toggleTruncation">Show more...</a></p>
+    </span>
+    <span v-else>
+        <p>{{ this.$props.value }} <a @click="toggleTruncation">Show less...</a></p>
+    </span>
 </template>
 
 <script>
-export default {
-    name: "TruncatedRecord",
-    props: {
-        truncated: String,
-        value: String,
-    },
-    data() {
-        return {
-            truncate: true,
-        }
-    },
-    methods: {
-        toggleTruncation() {
-            this.$data.truncate = !this.$data.truncate
+    export default {
+        name: "TruncatedRecord",
+        props: {
+            truncated: String,
+            value: String,
         },
-    },
-}
+        data() {
+            return {
+                truncate: true,
+            }
+        },
+        methods: {
+            toggleTruncation() {
+                this.$data.truncate = !this.$data.truncate
+            },
+        },
+    }
 </script>
