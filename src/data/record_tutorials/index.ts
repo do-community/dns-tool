4ae90d7415a126e16770a3ddfb55746815d2280f
Jake Gealer <jake@gealer.email>
2019-08-01 15:46:18 +0100

    Add namecheap registrar support

--- src/data/record_tutorials/index.ts
+++ src/data/record_tutorials/index.ts
@@ -5,7 +5,8 @@ import godaddy from "./godaddy"
 import googledomains from "./googledomains"
 import porkbun from "./porkbun"
 import networksolutions from "./network_solutions"
+import namecheapregistrar from "./namecheap_registrar"
 
 export default {
-    cloudflare, digitalocean, namecheap, godaddy, googledomains, porkbun, networksolutions,
+    cloudflare, digitalocean, namecheap, godaddy, googledomains, porkbun, networksolutions, namecheapregistrar,
 }
---
+++ src/data/record_tutorials/namecheap_registrar.ts
@@ -0,0 +1,1 @@
+export default "https://www.namecheap.com/support/knowledgebase/article.aspx/767/10/how-to-change-dns-for-a-domain"
