9792815e2f352c654334a51da69a706d75af27a1
MattIPv4 <matthew@cowley.org.uk>
2019-08-23 15:57:02 +0100

    Add template fetching to package scripts

--- build/fetchTemplate.js
+++ build/fetchTemplate.js
@@ -1,9 +1,26 @@
+/*
+Copyright 2019 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fetch = require('node-fetch')
 const fs = require('fs')
 
 const main = async () => {
+    console.log('Fetching Community Tools template from www.digitalocean.com...')
     const res = await fetch('https://www.digitalocean.com/community/tools?render_as_empty=1')
     let rawHTML = await res.text()
 
@@ -57,6 +74,7 @@ const main = async () => {
 
     // Export
     fs.writeFileSync(`${__dirname}/base.html`, rawHTML)
+    console.log('...fetching & conversion completed, saved to base.html')
 }
 
 main()
