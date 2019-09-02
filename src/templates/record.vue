b33f478746dc63a632df90a1ee59f4f34570e456
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2019-09-02 15:55:45 +0100

    Update SRV record handling (#72)
    * Parse SRV records
    
    * Add explainer if no SRV records found
    
    * Fix inline code styling

--- src/templates/record.vue
+++ src/templates/record.vue
@@ -29,6 +29,7 @@ limitations under the License.
             </p>
             <div v-if="recordKeys.length === 0">
                 <p><b>{{ i18n.templates.records.noRecords }}</b></p>
+                <p v-if="this.$props.recordType === 'SRV'" v-html="i18n.templates.records.srvFormat"></p>
             </div>
             <div v-else>
                 <table class="table">
