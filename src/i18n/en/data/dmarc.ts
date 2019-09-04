5b744784634bc1bae654bbff5f1b0c03d1f0b893
Jake Gealer <jake@gealer.email>
2019-09-04 16:19:47 +0100

    Change descriptions

--- src/i18n/en/data/dmarc.ts
+++ src/i18n/en/data/dmarc.ts
@@ -4,7 +4,7 @@ export default {
     rua: "This is a comma sepearated list of e-mails where aggregate reports should be sent.",
     pct: "This defines the percentage of e-mail which will have this policy applied.",
     p: "This defines how domain administrators want e-mail treated on the primary domain that fail the DMARC validation checks. The options are none (treat the same as usual), quarantine (mark as spam) or reject (reject the message).",
-    aspf: "If this is set to strict (s) mode the domain which e-mails are being sent to must match directly. If not, it will default to relaxed (r) mode where any sub-domain is fine.",
-    adkim: "If this is set to strict (s) mode the domain which e-mails are being sent from must match directly. If not, it will default to relaxed (r) mode where any sub-domain is fine.",
+    aspf: "If this is set to strict (s) mode the domain which e-mails are being sent to must match directly. If not, it will default to relaxed (r) mode where any sub-domain is fine. This will use the configuration from your SPF record.",
+    adkim: "If this is set to strict (s) mode the domain which e-mails are being sent from must match directly. If not, it will default to relaxed (r) mode where any sub-domain is fine. This will use the configuration from your DKIM record.",
     sp: "This defines how domain administrators want e-mail treated on sub-domains that fail the DMARC validation checks. The options are none (treat the same as usual), quarantine (mark as spam) or reject (reject the message).",
 } as {[key: string]: string}
