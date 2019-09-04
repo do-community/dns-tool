2a130e54008bd24d0732216364d13cc7e7ff26bb
Jake Gealer <jake@gealer.email>
2019-09-04 16:16:35 +0100

    Add SP

--- src/i18n/en/data/dmarc.ts
+++ src/i18n/en/data/dmarc.ts
@@ -3,7 +3,8 @@ export default {
     ruf: "This is a comma sepearated list of e-mails where forensic reports should be sent.",
     rua: "This is a comma sepearated list of e-mails where aggregate reports should be sent.",
     pct: "This defines the percentage of e-mail which will have this policy applied.",
-    p: "This defines how domain administrators want e-mail treated that fails the DMARC validation checks. The options are none (treat the same as usual), quarantine (mark as spam) or reject (reject the message).",
+    p: "This defines how domain administrators want e-mail treated on the primary domain that fail the DMARC validation checks. The options are none (treat the same as usual), quarantine (mark as spam) or reject (reject the message).",
     aspf: "If this is set to strict (s) mode the domain which e-mails are being sent to must match directly. If not, it will default to relaxed (r) mode where any sub-domain is fine.",
     adkim: "If this is set to strict (s) mode the domain which e-mails are being sent from must match directly. If not, it will default to relaxed (r) mode where any sub-domain is fine.",
+    sp: "This defines how domain administrators want e-mail treated on sub-domains that fail the DMARC validation checks. The options are none (treat the same as usual), quarantine (mark as spam) or reject (reject the message).",
 } as {[key: string]: string}
