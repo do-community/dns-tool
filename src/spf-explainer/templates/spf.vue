4d7259e0d7a6530fb1b116c2a23f3123b898d5dc
Jake Gealer <jake@gealer.email>
2019-09-13 16:19:09 +0100

    I'm not keeping this edit, I just want to test this

--- src/spf-explainer/templates/spf.vue
+++ src/spf-explainer/templates/spf.vue
@@ -18,7 +18,7 @@ limitations under the License.
     <div v-if="!loading">
         <h5 class="title is-5">
             <span v-for="(_, key) in links" :key="key" :ref="key">
-                <a @click="goToIndex(key)" @mouseover="markActive(key)">{{ key }}&nbsp;</a>
+                <a @click="goToIndex(key)" @mouseover="markActive(key)">{{ key }}</a>
             </span>
         </h5>
         <div v-for="part in parts" :key="part[0]">
