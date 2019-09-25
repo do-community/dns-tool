82446ee3ace206584a4d6c3cb95d847bd373838b
MattIPv4 <me@mattcowley.co.uk>
2019-09-25 11:09:54 +0100

    Use PSL for whoisjs to get root domain

--- src/dns-lookup/utils/whoisJS.ts
+++ src/dns-lookup/utils/whoisJS.ts
@@ -14,13 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import * as psl from "psl"
+
 export default async (name: string) => {
-    const nameSplit = name.split(".")
-    const ending = nameSplit.pop()!
-    const domain = nameSplit.pop()!
-    const domainFull = `${domain}.${ending}`
     return await fetch(
-        `https://whoisjs.com/api/v1/${encodeURIComponent(domainFull)}`,
+        `https://whoisjs.com/api/v1/${encodeURIComponent(psl.parse(name).domain)}`,
         {
             headers: {
                 Accept: "application/json",
