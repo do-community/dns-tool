5bd9aeb7d4f9f3fd2551053345a5b70b5ab85f28
MattIPv4 <me@mattcowley.co.uk>
2020-07-19 16:52:59 +0100

    Disable `@typescript-eslint/explicit-module-boundary-types`

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports =  {
     "@typescript-eslint/no-non-null-assertion": 0,
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
     "require-atomic-updates": 0,
     "no-undef": 0,
     "vue/require-v-for-key": 0,
